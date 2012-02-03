82c2790bc0b98238f21947db0982892e426e2057
Baochu Xu <bcxu@hzwowpad.com>
2012-02-03 17:52:10 +0800

    change .bashrc for vim c-c c-q

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #/bin/bash
 
-CURL_DIR=$(cd "$(dirname "$0")";pwd)
-
 echo "Install Vim......"
 
-sudo apt-get install vim vim-gtk gtk2-engines-pixbuf exuberant-ctags cscope -y 
+#sudo apt-get install vim vim-gtk gtk2-engines-pixbuf exuberant-ctags cscope -y 
 
+CURL_DIR=$(cd "$(dirname "$0")";pwd)
 ln -sf $CURL_DIR ~/.vim
 ln -sf $CURL_DIR/vimrc ~/.vimrc
+
+LINE=$(grep "stty -ixon" ~/.bashrc)
+if [ -z "$LINE" ]; then
+	echo "stty -ixon" >> ~/.bashrc
+fi
